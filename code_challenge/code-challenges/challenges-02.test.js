f62478c005d6fc5df6bf1de762d2a036570a689b
Ghadier-Alenezi <Ghadier.Mutar.elq.js@tuwaiq.edu.sa>
2021-12-10 00:48:01 +0300

    challenge 02 - 1 done

--- code_challenge/code-challenges/challenges-02.test.js
+++ code_challenge/code-challenges/challenges-02.test.js
@@ -9,7 +9,11 @@ Write a function named raisedToTheThird that takes in an array of numbers and re
 ------------------------------------------------------------------------------------------------ */
 
 const raisedToTheThird = (arr) => {
-  // Solution code here...
+  const newArr = [];
+  arr.forEach((element) => {
+    newArr.push(Math.pow(element, 3));
+  });
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
